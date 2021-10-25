68f4f27893fe88ef46fc628f226dd3029b3398ce
mozzar <realmozzar@gmail.com>
2021-10-25 15:44:58 +0200

    first edits

---
+++ src/Entity/ApplicationForm.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class ApplicationForm
+{
+    protected $leader;
+    protected $clubname;
+    protected $department;
+    protected $patron;
+
+    protected $name_surname;
+    protected $album_number;
+    protected $function;
+    protected $start_date;
+
+}
