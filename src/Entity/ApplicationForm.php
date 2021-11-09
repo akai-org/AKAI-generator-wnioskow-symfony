f69b9a0f481a267e4506fa194a10aa80c72fcfed
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-09 20:39:57 +0100

    add setters/getters in ApplicationForm

--- src/Entity/ApplicationForm.php
+++ src/Entity/ApplicationForm.php
@@ -16,6 +16,83 @@ class ApplicationForm
     protected $function;
     protected $start_date;
 
-    
+    public function getLeader()
+    {
+        return $this->leader;
+    }
 
+    public function setLeader($leader): void
+    {
+        $this->leader = $leader;
+    }
+
+    public function getClubname()
+    {
+        return $this->clubname;
+    }
+
+    public function setClubname($clubname): void
+    {
+        $this->clubname = $clubname;
+    }
+
+    public function getDepartment()
+    {
+        return $this->department;
+    }
+
+    public function setDepartment($department): void
+    {
+        $this->department = $department;
+    }
+
+    public function getPatron()
+    {
+        return $this->patron;
+    }
+
+    public function setPatron($patron): void
+    {
+        $this->patron = $patron;
+    }
+
+    public function getNameSurname()
+    {
+        return $this->name_surname;
+    }
+
+    public function setNameSurname($name_surname): void
+    {
+        $this->name_surname = $name_surname;
+    }
+
+    public function getAlbumNumber()
+    {
+        return $this->album_number;
+    }
+
+    public function setAlbumNumber($album_number): void
+    {
+        $this->album_number = $album_number;
+    }
+
+    public function getFunction()
+    {
+        return $this->function;
+    }
+
+    public function setFunction($function): void
+    {
+        $this->function = $function;
+    }
+
+    public function getStartDate()
+    {
+        return $this->start_date;
+    }
+
+    public function setStartDate($start_date): void
+    {
+        $this->start_date = $start_date;
+    }
 }
