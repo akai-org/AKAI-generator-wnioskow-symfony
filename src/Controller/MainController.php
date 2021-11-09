326ef910cda0f5026d4ce77bf199531a863d680d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-09 20:52:34 +0100

    render basic form and log the data

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,19 +3,40 @@
 
 namespace App\Controller;
 
+use App\Entity\ApplicationForm;
+use App\Form\ApplicationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/")
+ */
 class MainController extends AbstractController
 {
-
-    public function index():Response
+    /**
+     * @Route("", methods={"GET"})
+     */
+    public function index(): Response
     {
-        $test = 123456789;
-        return $this->render('base.html.twig', [
-            'test' => $test
+        $form = $this->createForm(ApplicationFormType::class, new ApplicationForm());
+        return $this->renderForm('base.html.twig', [
+            'form' => $form
         ]);
+    }
 
+    /**
+     * @Route("", methods={"POST"})
+     */
+    public function postForm(Request $request): Response
+    {
+        $form = $this->createForm(ApplicationFormType::class, new ApplicationForm());
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            error_log(var_export($form->getData(), true));
+        }
+        return $this->redirect("/");
     }
 
 }
